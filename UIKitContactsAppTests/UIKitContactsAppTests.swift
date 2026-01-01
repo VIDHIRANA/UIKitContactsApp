35d3a82c3db555863c24c9f0836e2167c69ab532
Vidhi Rana <vidhi.kvs.rana@gmail.com>
2026-01-01 23:42:22 +0530

    Initial Commit

---
+++ UIKitContactsAppTests/UIKitContactsAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  UIKitContactsAppTests.swift
+//  UIKitContactsAppTests
+//
+//  Created by Vidhi Rana on 01/01/26.
+//
+
+import Testing
+@testable import UIKitContactsApp
+
+struct UIKitContactsAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
